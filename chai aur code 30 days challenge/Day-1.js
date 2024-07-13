932e693ab6111a746602875f1dfd19dade6d0835
sadmanryanriad <sadmanryanriad@gmail.com>
2024-07-13 09:32:32 +0600

    Day-1

---
+++ chai aur code 30 days challenge/Day-1.js
@@ -0,0 +1,13 @@
+// Day-1: Variables and Data Types
+
+var num = 12;
+let str = "mitu";
+const bool = true;
+const obj = { name: "riad" };
+const arr = [1, 2, 3];
+
+console.log(typeof num); //number
+console.log(typeof str); //string
+console.log(typeof bool); //boolean
+console.log(typeof obj); //object
+console.log(typeof arr); //object
